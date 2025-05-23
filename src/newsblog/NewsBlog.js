f87577b8814d83a6e48973f3ed35b3d3f8251049
yongjinjj <yjnr10@gmail.com>
2025-05-23 14:52:35 +0900

    삭제 기능 추가

--- src/newsblog/NewsBlog.js
+++ src/newsblog/NewsBlog.js
@@ -116,6 +116,14 @@ function NewsBlog(){
 
                             }}>❤</span>{likeCountArr[index]}</h4>
                             <p>내용 무</p>
+                            <button onClick={()=>{
+                                let temp = [...news];
+                                temp.splice(index, 1);
+                                setNews(temp);
+
+                                likeCountArr.splice(index, 1);
+
+                            }}>삭제</button>
                         </div>
                     )
 
